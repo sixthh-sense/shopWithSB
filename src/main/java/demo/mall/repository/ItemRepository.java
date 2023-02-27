912ae3d3a7699968f68039bde934ec995904f1fd
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-02-27 18:25:15 +0900

    :itemDetail 의 콜론은 intelliJ쪽 변수라는 뜻? nativeQuery -> like %:itemDetail% 에서 빨간 오류 버튼이 IntelliJ에서 뜨는데, 오류가 뜨는데도 막상 실행 자체는 가능하고, 데이터 조회도 제대로 됨 오히려 IntelliJ에서 '올바른' query라고 지정한 '%:itemDetail%'을 했을 때 데이터 조회가 제대로 안 됨

--- src/main/java/demo/mall/repository/ItemRepository.java
+++ src/main/java/demo/mall/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package demo.mall.repository;
 
 import demo.mall.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -19,6 +21,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
+    // %itemDetail%이 아니라 %:itemDetail% -> : 콜론에 무슨 의미가 있는 걸까?
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
-
+    // native query : Item -> item, i -> *
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
