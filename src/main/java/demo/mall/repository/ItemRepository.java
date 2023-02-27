c967c69b8d3b9bdbf7fff245a01aff4806c8d44e
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-02-27 16:09:44 +0900

    test용 properties 설정 상품명으로 상품 찾는 메소드 repository에 생성(JPA) 관련 테스트 코드 작성

--- src/main/java/demo/mall/repository/ItemRepository.java
+++ src/main/java/demo/mall/repository/ItemRepository.java
@@ -3,5 +3,14 @@ package demo.mall.repository;
 import demo.mall.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    /*
+    * <S extends T> save(S entity)
+    * void delete(T entity)
+    * count()
+    * Iterable<T> findAll()
+    * */
+    List<Item> findByItemNm(String itemNm);
 }
