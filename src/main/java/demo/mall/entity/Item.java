5f803f07fd140be8c5ca9383f9c64d33dd4c16e2
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-07 13:15:30 +0900

    Item Entity class: 상품 데이터 업데이트 로직 생성

--- src/main/java/demo/mall/entity/Item.java
+++ src/main/java/demo/mall/entity/Item.java
@@ -1,6 +1,7 @@
 package demo.mall.entity;
 
 import demo.mall.constant.ItemSellStatus;
+import demo.mall.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @ToString
 @Entity
 @Table(name = "item")
-public class Item {
+public class Item extends BaseEntity{
 
     @Id
     @Column(name = "item_id")
@@ -49,4 +50,12 @@ public class Item {
     )
     private List<Member> member;
      */
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }
