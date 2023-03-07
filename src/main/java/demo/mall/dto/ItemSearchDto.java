e9ebbc77ed237e7f6ce27079aef40445c3636a69
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-07 13:46:48 +0900

    상품 데이터 조회시 상품 조회 조건 dto

---
+++ src/main/java/demo/mall/dto/ItemSearchDto.java
@@ -0,0 +1,15 @@
+package demo.mall.dto;
+
+import demo.mall.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+    private ItemSellStatus searchSellStatus;
+    private String searchBy;
+    private String searchQuery = "";
+}
