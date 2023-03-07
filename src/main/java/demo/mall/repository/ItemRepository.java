64de861e68e93ab8f3b2a500bd4a1a12aa10ef7e
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-07 16:09:29 +0900

    ItemRepositoryCustom 상속

--- src/main/java/demo/mall/repository/ItemRepository.java
+++ src/main/java/demo/mall/repository/ItemRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
     /*
     * <S extends T> save(S entity)
     * void delete(T entity)
