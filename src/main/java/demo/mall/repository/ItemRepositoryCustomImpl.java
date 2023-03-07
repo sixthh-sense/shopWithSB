49709059294cbc1e5a1d213b98f14ee7aa767af7
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-07 16:10:36 +0900

    사용자 정의 인터페이스(ItemRepositoryCustom) 구현 - Impl 표현 붙여줘야 정상 작동

---
+++ src/main/java/demo/mall/repository/ItemRepositoryCustomImpl.java
@@ -0,0 +1,83 @@
+package demo.mall.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import demo.mall.constant.ItemSellStatus;
+import demo.mall.dto.ItemSearchDto;
+import demo.mall.entity.Item;
+import demo.mall.entity.QItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory; // private -> private final
+
+    public ItemRepositoryCustomImpl(EntityManager em){
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    private BooleanExpression searchSellStatusEq(ItemSellStatus searchSellStatus){
+        return searchSellStatus == null ? null : QItem.item.itemSellStatus.eq(searchSellStatus);
+    }
+
+    private BooleanExpression regDtsAfter(String searchDateType){
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        if(StringUtils.equals("all", searchDateType) || searchDateType == null){
+            return null;
+        } else if(StringUtils.equals("1d", searchDateType)){
+            dateTime = dateTime.minusDays(1);
+        } else if(StringUtils.equals("1w", searchDateType)){
+            dateTime = dateTime.minusWeeks(1);
+        } else if(StringUtils.equals("1m", searchDateType)){
+            dateTime = dateTime.minusMonths(1);
+        } else if(StringUtils.equals("6m", searchDateType)){
+            dateTime = dateTime.minusMonths(6);
+        }
+
+        return QItem.item.regTime.after(dateTime);
+    }
+
+    private BooleanExpression searchByLike(String searchBy, String searchQuery){
+
+        if(StringUtils.equals("itemNm", searchBy)){
+            return QItem.item.itemNm.like("%" + searchQuery + "%");
+        } else if(StringUtils.equals("createdBy", searchBy)){
+            return QItem.item.createdBy.like("%" + searchQuery + "%");
+        }
+
+        return null;
+    }
+
+    @Override
+    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+
+        List<Item> content = queryFactory
+                .selectFrom(QItem.item)
+                .where(regDtsAfter(itemSearchDto.getSearchDateType()),
+                        searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
+                        searchByLike(itemSearchDto.getSearchBy(),
+                                itemSearchDto.getSearchQuery()))
+                .orderBy(QItem.item.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory.select(Wildcard.count).from(QItem.item)
+                .where(regDtsAfter(itemSearchDto.getSearchDateType()),
+                        searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
+                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()))
+                .fetchOne(); // may produce NullPonterException?
+
+        return new PageImpl<>(content, pageable, total);
+    }
+}
