a0da51a747dc5c1fcb2446893f8506766e8ecd98
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-07 13:48:05 +0900

    사용자 정의 인터페이스

---
+++ src/main/java/demo/mall/repository/ItemRepositoryCustom.java
@@ -0,0 +1,10 @@
+package demo.mall.repository;
+
+import demo.mall.dto.ItemSearchDto;
+import demo.mall.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+}
