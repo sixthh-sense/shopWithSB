8c1dfd2ebe613e9b3a718111bf8d043fb6ffd9ac
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 19:34:51 +0900

    JPQL 관련 java.lang.IllegalStateException 발생 -> Param annotation 추가

--- src/main/java/demo/mall/repository/CartItemRepository.java
+++ src/main/java/demo/mall/repository/CartItemRepository.java
@@ -4,6 +4,7 @@ import demo.mall.dto.CartDetailDto;
 import demo.mall.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +15,5 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     "from CartItem  ci, ItemImg im " + "join ci.item i " + "where ci.cart.id = :cartId "
     + "and im.item.id = ci.item.id " + "and im.repImgYn = 'Y' "
     + "order by ci.regTime desc")
-    List<CartDetailDto> findCartDetailDtoList(Long cartId);
+    List<CartDetailDto> findCartDetailDtoList(@Param("cartId") Long cartId);
 }
