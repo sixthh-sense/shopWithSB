d6c50c643550d5046a6899ab0c64bdb39c97b0b4
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 14:50:49 +0900

    주문 정보 dto 생성

---
+++ src/main/java/demo/mall/dto/OrderHistDto.java
@@ -0,0 +1,33 @@
+package demo.mall.dto;
+
+import demo.mall.constant.OrderStatus;
+import demo.mall.entity.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderHistDto {
+
+    private Long orderId;
+
+    private String orderDate;
+
+    private OrderStatus orderStatus;
+
+    public OrderHistDto(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
+    }
+
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>();
+
+    public void addOrderItemDto(OrderItemDto orderItemDto) {
+        orderItemDtoList.add(orderItemDto);
+    }
+}
