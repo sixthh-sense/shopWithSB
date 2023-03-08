79f7506dfa9596c5228747e6ed539ee9f0dceb5e
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 15:02:55 +0900

    구매이력 GET API 컨트롤러단 구현

--- src/main/java/demo/mall/controller/OrderController.java
+++ src/main/java/demo/mall/controller/OrderController.java
@@ -1,20 +1,24 @@
 package demo.mall.controller;
 
 import demo.mall.dto.OrderDto;
+import demo.mall.dto.OrderHistDto;
 import demo.mall.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -46,5 +50,18 @@ public class OrderController {
         }
 
         return new ResponseEntity<>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model){
+
+        Pageable pageable = PageRequest.of(page.orElse(0), 4);
+        Page<OrderHistDto> ordersHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", ordersHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHist";
     }
 }
