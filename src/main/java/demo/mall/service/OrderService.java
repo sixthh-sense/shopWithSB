13fb55f819bfbc3d14d22bdc6af7c714b4837abb
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 14:19:05 +0900

    주문 서비스단 생성, 메소드 추가 - 주문 처리 메소드 (상품 조회 -> 회원 정보 조회 -> 상품 엔티티 생성, 주문 엔티티 생성 -> 주문 엔티티 저장)

---
+++ src/main/java/demo/mall/service/OrderService.java
@@ -0,0 +1,48 @@
+package demo.mall.service;
+
+import demo.mall.entity.Item;
+import demo.mall.entity.Member;
+import demo.mall.entity.Order;
+import demo.mall.entity.OrderItem;
+import demo.mall.dto.OrderDto;
+import demo.mall.repository.ItemImgRepository;
+import demo.mall.repository.ItemRepository;
+import demo.mall.repository.MemberRepository;
+import demo.mall.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final ItemRepository itemRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final OrderRepository orderRepository;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long order(OrderDto orderDto, String email) {
+
+        Item item = itemRepository.findById(orderDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        Member member = memberRepository.findByEmail(email);
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
+        orderItemList.add(orderItem);
+        Order order = Order.createOrder(member, orderItemList);
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+}
