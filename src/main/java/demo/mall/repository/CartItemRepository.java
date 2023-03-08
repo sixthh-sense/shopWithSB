67d02cda0f84e8da25decfbb49fc2e9d6580fe69
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 18:15:12 +0900

    장바구니 수정, 삭제 관련 코드

--- src/main/java/demo/mall/repository/CartItemRepository.java
+++ src/main/java/demo/mall/repository/CartItemRepository.java
@@ -1,8 +1,18 @@
 package demo.mall.repository;
 
+import demo.mall.dto.CartDetailDto;
 import demo.mall.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    @Query("select new demo.mall.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+    "from CartItem  ci, ItemImg im " + "join ci.item i " + "where ci.cart.id = :cartId "
+    + "and im.item.id = ci.item.id " + "and im.repImgYn = 'Y' "
+    + "order by ci.regTime desc")
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
--- src/main/java/demo/mall/service/CartService.java
+++ src/main/java/demo/mall/service/CartService.java
@@ -1,5 +1,6 @@
 package demo.mall.service;
 
+import demo.mall.dto.CartDetailDto;
 import demo.mall.dto.CartItemDto;
 import demo.mall.entity.Cart;
 import demo.mall.entity.CartItem;
@@ -12,8 +13,11 @@ import demo.mall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +52,47 @@ public class CartService {
             cartItemRepository.save(cartItem);
             return cartItem.getId();
         }
+    }
+
+    @Transactional(readOnly = true)
+    public List<CartDetailDto> getCartList(String email){
+
+        List<CartDetailDto> cartDetailDtoList = new ArrayList<>();
+
+        Member member = memberRepository.findByEmail(email);
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if(cart == null){
+            return cartDetailDtoList;
+        }
+
+        cartDetailDtoList = cartItemRepository.findCartDetailDtoList(cart.getId());
+        return cartDetailDtoList;
+    }
+
+    @Transactional(readOnly = true)
+    public boolean validateCartItem(Long cartItemId, String email){
+        Member curMember = memberRepository.findByEmail(email);
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        Member savedMember = cartItem.getCart().getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void updateCartItemCount(Long cartItemId, int count){
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        cartItem.updateCount(count);
+    }
+
+    public void deleteCartItem(Long cartItemId) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        cartItemRepository.delete(cartItem);
     }
 }
