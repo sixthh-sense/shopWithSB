16e450839c1f6fa4f70e7a3fc9f5184f3e1a630f
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-03-08 19:33:45 +0900

    TemplateInputException 발생 - 컨트롤러단 재검사, 불필요한 / 제거

--- src/main/java/demo/mall/controller/ItemController.java
+++ src/main/java/demo/mall/controller/ItemController.java
@@ -31,7 +31,7 @@ public class ItemController {
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model) {
         model.addAttribute("itemFormDto", new ItemFormDto());
-        return "/item/itemForm";
+        return "item/itemForm";
     }
 
     @GetMapping(value = "admin/item/{itemId}")
